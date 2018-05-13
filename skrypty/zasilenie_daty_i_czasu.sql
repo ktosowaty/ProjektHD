59efaa1d47281f6cca01fd0e2bcb2afd1675129f
patrycha <33624126+patrycha@users.noreply.github.com>
2018-05-13 12:00:41 +0200

    Update zasilenie_daty_i_czasu.sql

--- skrypty/zasilenie_daty_i_czasu.sql
+++ skrypty/zasilenie_daty_i_czasu.sql
@@ -1,3 +1,5 @@
+delete date
+delete time
 --data
 declare @start_data  date;
 declare @end_data  date;
@@ -9,8 +11,8 @@ set @data=@start_data;
 
 while datediff(day,@data,@end_data)>-1
 begin
-insert into dbo.Date(YEAR,QUARTER,MONTH,WEEK,DAY,DOW) 
-values(convert(integer,DATEPART(YEAR,@DATA)),convert(integer,DATEPART(quarter,@DATA)),convert(integer,DATEPART(MONTH,@DATA)),convert(integer,DATEPART(week,@DATA)),convert(integer,DATEPART(DAY,@DATA)),convert(integer,DATEPART(weekday,@DATA)))
+insert into dbo.Date(YEAR,QUARTER,MONTH,WEEK,DAY,DOW,whole_data) 
+values(convert(integer,DATEPART(YEAR,@DATA)),convert(integer,DATEPART(quarter,@DATA)),convert(integer,DATEPART(MONTH,@DATA)),convert(integer,DATEPART(week,@DATA)),convert(integer,DATEPART(DAY,@DATA)),convert(integer,DATEPART(weekday,@DATA)), CONVERT(varchar, @data, 105))
 set @data=dateadd(day,1,@data)
 end 
 
@@ -21,5 +23,23 @@ while datepart(day,@time)<>'2'
 begin
 insert into dbo.time(hour_normal,hour_radio,minute) 
 values (datepart(hour,@time),datepart(hour,DATEADD(hour,2,@time)),datepart(minute,@time))
-set @time=dateadd(minute,1,@time)
 end 
+
+--zasilenie godziny radiowej
+UPDATE dbo.time  
+SET HOUR_RADIO =  CASE  
+    WHEN HOUR_NORMAL = 0 THEN 24 
+    WHEN HOUR_NORMAL = 1 THEN 25 
+	ELSE  HOUR_NORMAL
+END 
+
+--ustawianie flagi informującej czy jest to dzień następny
+UPDATE dbo.time  
+SET FLAG =  CASE  
+	WHEN HOUR_NORMAL = 0 OR HOUR_NORMAL = 1 THEN 1 
+	ELSE 0
+    END 
+
+--zasilenie całkowitego czasu
+UPDATE dbo.time
+set whole_time = (CONVERT(VARCHAR,HOUR_RADIO)+':'+ SUBSTRING('0'+CONVERT(VARCHAR,MINUTE),LEN('0'+CONVERT(VARCHAR,MINUTE))-1,2)+':00')
